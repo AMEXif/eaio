00f51a6af486ed8b552a7b3b8cd0b1abd52d3709
AMEXif <67397386+AMEXif@users.noreply.github.com>
2022-03-18 18:36:52 +0100

    Uploaded source code

---
+++ src/MVVM/View/HomeView.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+
+namespace eaio.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for HomeView.xaml
+    /// </summary>
+    partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+    public void openDiscord(object sender, MouseButtonEventArgs e)
+        {
+
+            string InvLink = "https://discord.gg/qnHAxyxcV5";
+            string command = $"/C start {InvLink}";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.WorkingDirectory = @"C:\";
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.Arguments = command;
+            process.Start();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void openGithub(object sender, MouseButtonEventArgs e)
+        {
+            string InvLink = "https://github.com/AMEXif/eaio";
+            string command = $"/C start {InvLink}";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.WorkingDirectory = @"C:\";
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.Arguments = command;
+            process.Start();
+        }
+
+        private void openCarrd(object sender, MouseButtonEventArgs e)
+        {
+            string InvLink = "https://amexif.carrd.co/";
+            string command = $"/C start {InvLink}";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.WorkingDirectory = @"C:\";
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.Arguments = command;
+            process.Start();
+        }
+    }
+}
+
---
+++ src/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using eaio.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eaio.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand TranscoderViewCommand { get; set; }
+
+        public RelayCommand InterpolationViewCommand { get; set; }
+
+        public RelayCommand UpscalerViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public TranscoderViewModel TranscoderVM { get; set; }
+
+        public InterpolationViewModel InterpolationVM { get; set; }
+
+        public UpscalerViewModel UpscalerVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            TranscoderVM = new TranscoderViewModel();
+            InterpolationVM = new InterpolationViewModel();
+            UpscalerVM = new UpscalerViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            TranscoderViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TranscoderVM;
+            });
+
+            InterpolationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = InterpolationVM;
+            });
+
+            UpscalerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = UpscalerVM;
+            });
+        }
+    }
+}
